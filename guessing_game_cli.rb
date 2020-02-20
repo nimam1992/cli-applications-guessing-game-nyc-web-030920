59ed8fbee723d15e9e7bdf32a1d3993f01d1c4a6
Nima Moshtagh <nima.moshtagh1@gmail.com>
2020-02-20 03:31:34 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -16,7 +16,9 @@ def run_guessing_game()
   random_num()
   if user_number == random_num()
     puts 'You guessed the correct number!'
-  else
+  elsif user_number != user_number
     puts "Sorry! The computer guessed #{random}."
+  elsif user_number == 'exit'
+    puts 'Goobye!'
   end
 end
